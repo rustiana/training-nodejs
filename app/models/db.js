c3b82f3a917b029c32c9b1088edce4303772f23e
Deden Rustiana <rustianadeden25@gmail.com>
2022-12-26 22:37:14 +0700

    fixed: error connection database

--- app/models/db.js
+++ app/models/db.js
@@ -4,13 +4,13 @@ const dbConfig = require("../config/db.config")
 const connection = mysql.createConnection({
     host: dbConfig.HOST,
     user: dbConfig.USER,
-    password: dbConfig,
+    password: dbConfig.PASSWORD,
     database: dbConfig.DB
 })
 
 connection.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
-  });
+    if (error) throw error
+    console.log("Successfully connected to the database.")
+})
   
 module.exports = connection;
